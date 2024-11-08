8ef00af23bb6307044df89e4aa3bc82333e492ab
Lorenzo Martini <33097135+Martini991@users.noreply.github.com>
2024-11-08 10:06:21 +0100

    Update docker-reproduce-bug.sh
    removed mapping to port 80, not needed

--- docker-reproduce-bug.sh
+++ docker-reproduce-bug.sh
@@ -11,5 +11,4 @@ docker run -it --rm \
   --env-file "${SCRIPT_DIR}/.env" \
   -w /run/wrong_user_manager/ \
   -v "${SCRIPT_DIR}:/run/wrong_user_manager/" \
-  -p "80:80" \
   wrong_user_manager:latest django-admin dumpdata child_app.item --all --traceback -o /dev/null
